b41e60ff0f00e08c6ba48fa04dedf2895742765c
北岡徹平 <teppei997@gmail.com>
2020-05-18 14:30:46 +0900

    新規

---
+++ app.js
@@ -0,0 +1,24 @@
+$(function(){
+    $('.header-right li').mouseenter(function(){
+        $(this).siblings().find('ul').slideUp();
+        $(this).children().slideDown('fast');
+        
+    });
+
+    $('html').click(function(){
+        $('.header-right ul li').slideUp();
+       
+       
+    })
+    $('body').bgSwitcher({
+        images: ['img/bannri.JPG','img/statue.JPG','img/canada.JPG'], // 切り替える背景画像を指定
+        interval:2000,
+    })
+
+
+
+})
+
+
+
+
